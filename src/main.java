9c69e16e874fbfc1fff44d967a5dc4768339a5b7
M-Ahmd <ma09500@gmail.com>
2025-07-04 18:00:53 +0300

    Add product class and update main method to utilize product features

--- src/main.java
+++ src/main.java
@@ -1,8 +1,16 @@
 //import java.util.*;
+import java.time.LocalDate;
 public class main {
-    public static void main(){
-        System.out.println("Hello wolrd");
-        String name = "Hamid";
-        System.out.println(name);
+    public static void main(String[] args){
+        products p = new products("Product1", 100, 2);
+        p.set_will_it_expire(true);
+        p.set_time_expire(LocalDate.of(2024, 12, 31));
+        p.set_is_shipping(true);
+        p.set_weight(1.5);
+        System.out.println("Product Name: " + p.get_name());
+        System.out.println("Product Price: " + p.get_price());
+        System.out.println("Product Quantity: " + p.get_quantity());
+        System.out.println("Product Expiration Date: " + p.get_time_expire());
+        System.out.println("Product Weight: " + p.get_weight());
     }
 }
---
+++ src/products.java
@@ -0,0 +1,105 @@
+import java.time.LocalDate;
+
+public class products {
+    String name;
+    int price;
+    int quantity;
+    boolean will_it_expire;
+    LocalDate time_expire;
+    boolean is_shipping;
+    double weight;
+
+    /* Constructor - to construct the complete product object
+     * @param name Name of the product
+     * @param price Price of the product
+     * @param quantity Quantity of the product
+     * @param will_it_expire Whether the product will expire
+     * @param time_expire Expiration date of the product
+     * @param is_shipping Whether the product is for shipping
+     * @param weight Weight of the product
+     */
+    public products(String name, int price, int quantity, boolean will_it_expire, LocalDate time_expire, boolean is_shipping, double weight)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.will_it_expire = will_it_expire;
+        this.time_expire = time_expire;
+        this.is_shipping = is_shipping;
+        this.weight = weight;
+    }
+    /* Constructor - to construct a partial product object */
+    public products(String name, int price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.will_it_expire = false; // Default value
+        this.time_expire = null; // Default value
+        this.is_shipping = false; // Default value
+        this.weight = 0.0; // Default value
+    }
+    /* Setter for price */
+    public void set_price(int price)
+    {
+        this.price = price;
+    }
+    /* Setter for quantity */
+    public void set_quantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+    /* Setter for expiration state */
+    public void set_will_it_expire(boolean will_it_expire)
+    {
+        this.will_it_expire = will_it_expire;
+    }
+    /* Setter for expiration date */
+    public void set_time_expire(LocalDate time_expire)
+    {
+        if(will_it_expire) {
+            this.time_expire = time_expire;
+        } else {
+            throw new IllegalArgumentException("Cannot set expiration date if product does not expire.");
+        }
+    }
+    /* Setter for shipping state */
+    public void set_is_shipping(boolean is_shipping)
+    {
+        this.is_shipping = is_shipping;
+    }
+    /* Setter for weight */
+    public void set_weight(double weight)
+    {
+        if(is_shipping) {
+            this.weight = weight;
+        } else {
+            throw new IllegalArgumentException("Cannot set weight if product is not for shipping.");
+        }
+    }
+    /* Getter for name */
+    public String get_name()
+    {
+        return name;
+    }
+    /* Getter for price */
+    public int get_price()
+    {
+        return price;
+    }
+    /* Getter for quantity */
+    public int get_quantity()
+    {
+        return quantity;
+    }
+    /* Getter for expiration date */
+    public LocalDate get_time_expire()
+    {
+        return time_expire;
+    }
+    /* Getter for weight */
+    public double get_weight()
+    {
+        return weight;
+    }
+}
