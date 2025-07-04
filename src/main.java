ad1d221c37274bf5016c6958c8186fcd730e09c2
M-Ahmd <ma09500@gmail.com>
2025-07-04 17:31:15 +0300

    init commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+//import java.util.*;
+public class main {
+    public static void main(){
+        System.out.println("Hello wolrd");
+        String name = "Hamid";
+        System.out.println(name);
+    }
+}
